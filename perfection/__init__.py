1db03c6400429097eacc9aa0b90b7291f13e0359
Eddie Antonio Santos <easantos@ualberta.ca>
2014-08-30 19:20:32 -0600

    Preparing for a major version bump...

--- perfection/__init__.py
+++ perfection/__init__.py
@@ -17,5 +17,5 @@ though by no means is it impossible. See for example:
 
 from .perfection import make_hash, make_dict, hash_parameters
 
-__version__ = '1.0.2'
+__version__ = '2.0.0-beta'
 __all__ = ['make_hash', 'make_dict', 'hash_parameters']
