ae1cfc3952f65aa714bcfd65da6b989f893c5bd0
Eddie Antonio Santos <easantos@ualberta.ca>
2014-09-02 23:58:03 -0600

    Started class-based builder refactor of Czech hash.

--- perfection/czech.py
+++ perfection/czech.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Use the Czech et al. method for generating minimal perfect hashes for strings. 
+Use the Czech et al. method for generating minimal perfect hashes for strings.
 """
 
 import random
@@ -9,8 +9,149 @@ import collections
 import forest
 
 
+__all__ = ['hash_info', 'create_hash', 'make_dict']
 
-HashInfo = collections.namedtuple('HashInfo', 't1 t2 g indices')
+
+_info_fields = ('t1', 't2', 'g', 'indicies')
+HashInfo = collections.namedtuple('HashInfo', _info_fields)
+
+
+class CzechHashBuilder(object):
+    """
+    A helper class that groups all data needed to create a Czech hash.
+
+    The entire hash algoritm occurs in  __init__.
+    """
+
+    def __init__(self, words, minimize=False):
+        # Store the words as an immutable sequence.
+        self.words = tuple(words)
+
+        # TODO: Index minimization
+        self.indicies = range(len(words[0]))
+
+        # Each of the following steps add fields to `self`:
+
+        # Mapping step:
+        #  - n, t1, t2, f1, f2, graph
+        self.generate_acyclic_graph()
+        # Assignment step:
+        #  - g
+        self.assign()
+
+        # Now hash_info will return the approriate object.
+
+    @property
+    def hash_info(self):
+        """
+        HashInfo tuple for the created hash.
+        """
+        return HashInfo(*(getattr(self, key) for key in _info_fields))
+
+    # Algorithm steps.
+
+    def generate_acyclic_graph(self):
+        """
+        Generates an acyclic graph for the given words.
+        Returns graph, and a list of edge-word associations.
+        """
+
+        # Maximum length of each table, respectivly.
+        # Hardcoded n = cm, where c = 3
+        # There might be a good way to choose an appropriate C,
+        # but [1] suggests the average amount of iterations needed
+        # to generate an acyclic graph is sqrt(3).
+        self.n = 3 * len(self.words)
+
+        max_tries = len(self.words) ** 2
+        for trial in xrange(max_tries):
+            try:
+                self.generate_or_fail()
+            except forest.InvariantError:
+                continue
+            else:
+                self.trials_taken = trial + 1
+                break
+
+    def generate_random_table(self):
+        """
+        Generates random tables for given word lists.
+        """
+        table = range(0, self.n)
+        random.shuffle(table)
+        return table
+
+    def generate_or_fail(self):
+        """
+        Attempts to generate a random acylic graph, raising an
+        InvariantError if unable to.
+        """
+
+        t1 = self.generate_random_table()
+        t2 = self.generate_random_table()
+        f1 = self.generate_func(t1)
+        f2 = self.generate_func(t2)
+        edges = [(f1(word), f2(word)) for word in self.words]
+
+        # Try to generate that graph, mack!
+        # Note that failure to generate the graph here should be caught
+        # by the caller.
+        graph = forest.ForestGraph(edges=edges)
+
+        # Associate each edge with its corresponding word.
+        associations = {}
+        for num in xrange(len(self.words)):
+            edge = edges[num]
+            word = self.words[num]
+            associations[graph.canonical_order(edge)] = (num, word)
+
+        # Assign all of these to the object.
+        for name in ('t1', 't2', 'f1', 'f2', 'graph', 'associations'):
+            self.__dict__[name] = locals()[name]
+
+    def generate_func(self, table):
+        """
+        Generates a random table based mini-hashing function.
+        """
+
+        # Ensure that `self` isn't suddenly in the closure...
+        n = self.n
+
+        def func(word):
+            return sum(x * ord(c) for x, c in zip(table, word)) % n
+
+        return func
+
+    def assign(self):
+        # Create an vector of empty assignments.
+        self.g = [None] * (self.n + 1)
+
+        # Assign all vertices.
+        for vertex in self.graph.vertices:
+            assert isinstance(vertex, int) and vertex <= self.n
+            # This vertex has already been assigned.
+            if self.g[vertex] is not None:
+                continue
+
+            self.g[vertex] = 0
+            self.assign_vertex(vertex)
+
+
+    def assign_vertex(self, vertex,):
+        for neighbour in self.graph.neighbours(vertex):
+            if self.g[neighbour] is not None:
+                # This neighbour has already been assigned.
+                continue
+
+            # Get the associated edge number
+            edge = self.graph.canonical_order((vertex, neighbour))
+            num, _word = self.associations[edge]
+
+            # Assign this vertex such that
+            # h(word) == g(vertex) + g(neighbour)
+            self.g[neighbour] = num - self.g[vertex]
+            self.assign_vertex(neighbour)
+
 
 
 def hash_parameters(words, minimize_indices=False):
@@ -19,135 +160,31 @@ def hash_parameters(words, minimize_indices=False):
 
     >>> info = hash_parameters('sun mon tue wed thu fri sat'.split())
     >>> len(info.t1)
-    3
-    >>> len(info.t2)
-    3
-    >>> len(info.g)
     21
+    >>> len(info.t2)
+    21
+    >>> len(info.g) # g values are 1-indexed...
+    22
     """
     # Ensure that we have an indexable sequence.
     words = tuple(words)
 
-    # generate acyclic graph should... generate the tables...
-    graph, associations = generate_acyclic_graph(words)
-    g = assign(graph, associations)
+    # Delegate to the hash builder.
+    return CzechHashBuilder(words).hash_info
 
-    return HashInfo(t1=(), t2=(), g=g, indices=None)
-
-
-def create_hash(words):
-    return
-
-
-def generate_random_table(words, n):
-    """
-    Generates random tables for given word lists.
-    """
-    table = range(0, n)
-    random.shuffle(table)
-    return table
-
-
-def generate_func(words, n):
-    """
-    Generates a random table based mini-hashing function.
-    """
-    table = generate_random_table(words, n)
-
-    def func(word):
-        return sum(x * ord(c) for x, c in zip(table, word)) % n
-
-    func.table = table
-    return func
-
-
-def generate_or_fail(words):
-    # TODO: Return the tables! 
-    # TODO: get the association step out of here!
-
-    # Hardcoded n = cm, where c = 3
-    # There might be a good way to choose an appropriate C,
-    # but [1] suggests the average amount of iterations needed
-    # to generate an acyclic graph is sqrt(3).
-    n = 3 * len(words)
-    f1 = generate_func(words, n)
-    f2 = generate_func(words, n)
-
-    edges = [(f1(word), f2(word)) for word in words]
-
-    # Try to generate that graph, mack!
-    try:
-        graph = forest.ForestGraph(edges=edges)
-    except forest.InvariantError:
-        return None
-
-    # Associate each edge with its corresponding word.
-    associations = {}
-    for num in xrange(len(words)):
-        edge = edges[num]
-        word = words[num]
-        associations[graph.canonical_order(edge)] = (num, word)
-
-    return graph, associations
-
-def generate_acyclic_graph(words):
-    """
-    Generates an acyclic graph for the given words.
-    Returns graph, and a list of edge-word associations.
-    """
-
-    max_tries = len(words) ** 2
-
-    for trial in xrange(max_tries):
-        value = generate_or_fail(words)
-        if value is None:
-            continue
-        return value
-
-def assign(graph, associations):
-    # TODO: pass n to this as an argument
-    n = max(graph.vertices)
-
-    # Create an vector of empty assignments.
-    g = [None] * (n + 1)
-
-    # Assign all vertices.
-    for vertex in graph.vertices:
-        assert isinstance(vertex, int) and vertex <= n
-        # This vertex has already been assigned.
-        if g[vertex] is not None:
-            continue
-
-        g[vertex] = 0
-        assign_vertex(vertex, graph, associations, g)
-
-    return g
-
-def assign_vertex(vertex, graph, associations, g):
-    for neighbour in graph.neighbours(vertex):
-        if g[neighbour] is not None:
-            # This neighbour has already been assigned.
-            continue
-
-        # Get the associated edge number
-        edge = graph.canonical_order((vertex, neighbour))
-        num, _word = associations[edge]
-
-        # Assign this vertex such that
-        # h(word) == g(vertex) + g(neighbour)
-        g[neighbour] = num - g[vertex]
-        assign_vertex(neighbour, graph, associations, g)
 
 
 def do_example():
     import keyword
     words = keyword.kwlist
 
-    graph, associations = generate_acyclic_graph(words)
-    print
-    print graph.to_dot(edge_labels={
-        edge: '%d: %s' % assoc for edge, assoc in associations.items()
-    })
+    hb = CzechHashBuilder(words)
     
+
+    print '/*', hb.t1, hb.t2, hb.g, '*/'
+    print hb.graph.to_dot(edge_labels={
+        edge: '%d: %s' % assoc for edge, assoc in hb.associations.items()
+    })
+
 if __name__ == '__main__':
     do_example()
