dc324202e11dcd18629853c3ce5aff0b9f11e5f8
Eddie Antonio Santos <easantos@ualberta.ca>
2014-09-04 13:42:13 -0600

    Added remaining API functions for Czech hash.

--- perfection/czech.py
+++ perfection/czech.py
@@ -6,21 +6,24 @@ Use the Czech et al. method for generating minimal perfect hashes for strings.
 import random
 import collections
 
-import forest
+# Hmm...
+from . import forest
+from . import utils
 
 
-__all__ = ['hash_info', 'create_hash', 'make_dict']
+__all__ = ['hash_parameters', 'make_hash', 'make_dict']
 
 
-_info_fields = ('t1', 't2', 'g', 'indicies')
+_info_fields = ('t1', 't2', 'g', 'indices')
 HashInfo = collections.namedtuple('HashInfo', _info_fields)
 
 
 class CzechHashBuilder(object):
     """
-    A helper class that groups all data needed to create a Czech hash.
+    A helper class that (iteratively) stores all data needed to create a Czech
+    hash.
 
-    The entire hash algoritm occurs in  __init__.
+    The entire hash generation algorithm occurs in  __init__.
     """
 
     def __init__(self, words, minimize=False):
@@ -28,7 +31,7 @@ class CzechHashBuilder(object):
         self.words = tuple(words)
 
         # TODO: Index minimization
-        self.indicies = range(len(words[0]))
+        self.indices = range(len(words[0]))
 
         # Each of the following steps add fields to `self`:
 
@@ -39,7 +42,7 @@ class CzechHashBuilder(object):
         #  - g
         self.assign()
 
-        # Now hash_info will return the approriate object.
+        # Now hash_info will return the appropriate object.
 
     @property
     def hash_info(self):
@@ -47,6 +50,31 @@ class CzechHashBuilder(object):
         HashInfo tuple for the created hash.
         """
         return HashInfo(*(getattr(self, key) for key in _info_fields))
+
+    @property
+    def hash_function(self):
+        """
+        Returns the hash function proper. Ensures that `self` is not bound to
+        the returned closure.
+        """
+        assert hasattr(self, 'f1') and hasattr(self, 'f2')
+
+        # These are not just convenient aliases for the given
+        # attributes; if `self` would creep into the returned closure,
+        # that would ensure that a reference to this big, fat object
+        # would be kept alive; hence, any hash function would carry
+        # around all of the auxiliary state that was created during the
+        # creation of the hash parameters.  Omitting `self` ensures this
+        # object has a change to be garbage collected.
+        f1, f2, g = self.f1, self.f2, self.g
+
+        def czech_hash(word):
+            v1 = f1(word)
+            v2 = f2(word)
+            return g[v1] + g[v2]
+
+        return czech_hash
+
 
     # Algorithm steps.
 
@@ -56,7 +84,7 @@ class CzechHashBuilder(object):
         Returns graph, and a list of edge-word associations.
         """
 
-        # Maximum length of each table, respectivly.
+        # Maximum length of each table, respectively.
         # Hardcoded n = cm, where c = 3
         # There might be a good way to choose an appropriate C,
         # but [1] suggests the average amount of iterations needed
@@ -83,7 +111,7 @@ class CzechHashBuilder(object):
 
     def generate_or_fail(self):
         """
-        Attempts to generate a random acylic graph, raising an
+        Attempts to generate a random acyclic graph, raising an
         InvariantError if unable to.
         """
 
@@ -136,7 +164,6 @@ class CzechHashBuilder(object):
             self.g[vertex] = 0
             self.assign_vertex(vertex)
 
-
     def assign_vertex(self, vertex,):
         for neighbour in self.graph.neighbours(vertex):
             if self.g[neighbour] is not None:
@@ -152,7 +179,7 @@ class CzechHashBuilder(object):
             self.g[neighbour] = num - self.g[vertex]
             self.assign_vertex(neighbour)
 
-
+# API functions  ##############################################################
 
 def hash_parameters(words, minimize_indices=False):
     """
@@ -173,18 +200,96 @@ def hash_parameters(words, minimize_indices=False):
     return CzechHashBuilder(words).hash_info
 
 
+def make_hash(words, *args, **kwargs):
+    """
+    Creates an ordered, minimal perfect hash function for the given sequence
+    of words.
+
+    >>> hf = make_hash(['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'])
+    >>> hf('fri')
+    5
+    >>> hf('sun')
+    0
+    """
+
+    # Use the hash builder proper, because HashInfo is assumed to not
+    # have the precious f1, and f2 attributes.
+    info = CzechHashBuilder(words, *args, **kwargs)
+
+    return info.hash_function
+
+
+def make_dict(name, words, *args, **kwargs):
+    """
+    make_dict(name, words, *args, **kwargs) -> mapping subclass
+
+    Takes a sequence of words (or a pre-built Czech HashInfo) and returns a
+    mapping subclass called `name` (used a dict) that employs the use of the
+    minimal perfect hash.
+
+    This mapping subclass has guaranteed O(1) worst-case lookups, additions,
+    and deletions, however is slower than dict() in practice.
+
+    >>> months = 'jan feb mar apr may jun jul aug sep oct nov dec'.split()
+    >>> MyDict = make_dict('MyDict', months) 
+    >>> d = MyDict(dec=21, feb=None, may='hello')
+    >>> d['jul'] = False
+    >>> d
+    MyDict([('feb', None), ('may', 'hello'), ('jul', False), ('dec', 21)])
+    >>> del d['may']
+    >>> del d['apr']
+    Traceback (most recent call last):
+    ...
+    KeyError: 'apr'
+    >>> len(d)
+    3
+    """
+
+    info = CzechHashBuilder(words, *args, **kwargs)
+
+    # Create a docstring that at least describes where the class came from...
+    doc = """
+        Dictionary-like object that uses minimal perfect hashing, perserving
+        original order. This class was generated by `%s.%s(%r, ...)`.
+        """ % (__name__, make_dict.__name__, name)
+
+    # Delegate to create_dict.
+    return utils.create_dict_subclass(name, info.hash_function, info.words, doc)
+
+
+def to_hash_info(unknown):
+    if isinstance(unknown, HashInfo) or isinstance(unknown, CzechHashBuilder):
+        # Unknown is a CzechHash.
+        return unknown
+    return HashInfo(unknown)
+
+    # Alternative method:
+    def _exception_based():
+        try:
+            (unknown.t1, unknown.t2, unknown.f1, unknown.f2, unknown.g)
+        except AttributeError:
+            return CreateCzechHash(unknown)
+        try:
+            for attr in ('t1', 't2', 'f1', 'f2', 'g'):
+                iter(getattr(unknown, attr))
+        except TypeError:
+            return CreateCzechHash(unknown)
+
+        return unknown
+
+
 
 def do_example():
     import keyword
     words = keyword.kwlist
 
     hb = CzechHashBuilder(words)
-    
 
     print '/*', hb.t1, hb.t2, hb.g, '*/'
     print hb.graph.to_dot(edge_labels={
         edge: '%d: %s' % assoc for edge, assoc in hb.associations.items()
     })
+
 
 if __name__ == '__main__':
     do_example()
---
+++ perfection/utils.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+"""
+Shared utilities for perfect hash tools.
+"""
+
+import collections
+
+def create_dict_subclass(name, hash_func, slots, doc):
+    """
+    Creates a dict subclass named name, using the hash_function to index
+    hash_length items. Doc should be any additional documentation added to the
+    class.
+    """
+
+    hash_length = len(slots)
+
+    # Returns array index -- raises a KeyError if the key does not match
+    # its slot value.
+    def index_or_key_error(key):
+        index = hash_func(key)
+        # Make sure the key is **exactly** the same.
+        if key != slots[index]:
+            raise KeyError(key)
+        return index
+
+    def init(self, *args, **kwargs):
+        self._arr = [None] * hash_length
+        self._len = 0
+
+        # Delegate initialization to update provided by MutableMapping:
+        self.update(*args, **kwargs)
+
+    def getitem(self, key):
+        index = index_or_key_error(key)
+        if self._arr[index] is None:
+            raise KeyError(key)
+        return self._arr[index][1]
+
+    def setitem(self, key, value):
+        index = index_or_key_error(key)
+        self._arr[index] = (key, value)
+
+    def delitem(self, key):
+        index = index_or_key_error(key)
+        if self._arr[index] is None:
+            raise KeyError(key)
+        self._arr[index] = None
+
+    def dict_iter(self):
+        return (pair[0] for pair in self._arr if pair is not None)
+
+    def dict_len(self):
+        # TODO: Make this O(1) using auxiliary state?
+        return sum(1 for _ in self)
+
+    def dict_repr(self):
+        arr_repr = (repr(pair) for pair in self._arr if pair is not None)
+        return ''.join((name, '([', ', '.join(arr_repr), '])'))
+
+    # Inheriting from MutableMapping gives us a whole whackload of methods for
+    # free.
+    bases = (collections.MutableMapping,)
+
+    return type(name, bases, {
+        '__init__': init,
+        '__doc__': doc,
+
+        '__getitem__': getitem,
+        '__setitem__': setitem,
+        '__delitem__': delitem,
+        '__iter__': dict_iter,
+        '__len__': dict_len,
+
+        '__repr__': dict_repr,
+    })
+
+
