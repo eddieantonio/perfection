bbc252f8ca7c5c61420a57a3b08681df4714a76b
Eddie Antonio Santos <easantos@ualberta.ca>
2016-11-18 12:35:41 -0700

    I probably should have done this work in a branch...

--- tests/test_czech.py
+++ tests/test_czech.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+from math import ceil
+
 from perfection.czech import CzechHashBuilder
 
-def test_guarentees():
+def test_guarantees():
     duplicated_input = 'guacala'
     unique_len = len(set(duplicated_input))
     info = CzechHashBuilder(duplicated_input)
     assert 2 * unique_len <= info.n <= 3 * unique_len
-    assert info.trials_taken <= info.n ** 0.5
+    assert info.trials_taken <= ceil(info.n ** 0.5)
